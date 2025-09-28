0b9b5a94edf65b9685d0f42bcfcf761f3c99dca7
kollisaivenu <ksaivenu2010@gmail.com>
2025-09-28 01:49:10 -0600

    added main.rs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::path::Path;
+use std::time::Instant;
+use GraphCut::algorithms::{MultiLevelPartitioner};
+use GraphCut::gen_weights::gen_uniform_weights;
+use GraphCut::imbalance::imbalance;
+use GraphCut::io::read_matrix_market_as_graph;
+use GraphCut::Partition;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let graph = read_matrix_market_as_graph(Path::new("./testdata/vt2010.mtx"));
+    let weights = gen_uniform_weights(graph.len());
+    let mut partition = vec![0; graph.len()];
+    let start = Instant::now();
+    MultiLevelPartitioner {jet_iterations: 12, jet_tolerance_factor: 0.999, ..Default::default()}.partition(&mut partition, (graph.clone(), &weights))?;
+    let elapsed_time = start.elapsed();
+    let edge_cut = graph.edge_cut(&partition);
+    let imbalance_of_partition = imbalance(2, &partition, weights.clone());
+    println!("Edge cut {:?}", edge_cut);
+    println!("Imbalance {:?}", imbalance_of_partition);
+    println!("Execution time {:?}", elapsed_time);
+    Ok(())
+}
