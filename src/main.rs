a16bce490fc27862244d83b9a82ad1942f9d3cf8
kollisaivenu <ksaivenu2010@gmail.com>
2026-03-25 11:04:27 -0600

    minor fix to main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ struct Args {
     /// Path of the .mtx file
     mtx_filepath: String,
 
-    /// Number of Partitions
-    num_of_partitions: usize,
+    /// Number of Parts
+    num_of_parts: usize,
 
     /// Imbalance Ratio
     balance_factor: f64,
@@ -43,14 +43,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut partition = vec![0; graph.len()];
     let start = Instant::now();
     MultiLevelPartitioner { balance_factor: args.balance_factor,
-                            num_of_partitions: args.num_of_partitions,
+                            num_of_parts: args.num_of_parts,
                             seed: None,
                             jet_iterations: args.iterations,
                             jet_tolerance_factor: args.tolerance_factor,
                             jet_filter_ratio: args.filter_ratio}.partition(&mut partition, (graph.clone(), &weights))?;
     let elapsed_time = start.elapsed();
     let edge_cut = graph.edge_cut(&partition);
-    let imbalance_of_partition = imbalance(args.num_of_partitions, &partition, &weights);
+    let imbalance_of_partition = imbalance(args.num_of_parts, &partition, &weights);
     write_partition_data_to_file(&partition, &args.partition_file)?;
     println!("Edge cut {:?}", edge_cut);
     println!("Imbalance {:?}", imbalance_of_partition);
